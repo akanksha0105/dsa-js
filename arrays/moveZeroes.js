a8b13687248962fc3c9cb459d979dcfe379c745d
Akanksha <mittalakanksha0105@gmail.com>
2026-01-13 14:50:44 +0530

    13. Move Zeroes

---
+++ arrays/moveZeroes.js
@@ -0,0 +1,15 @@
+//T.C. : O(N)
+//S.C. : O(1)
+
+const moveZeroes = (nums) => {
+	let x = 0;
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[i] !== 0) {
+			nums[x] = nums[i];
+			x++;
+		}
+	}
+	for (let i = x; i < nums.length; i++) {
+		nums[i] = 0;
+	}
+};
