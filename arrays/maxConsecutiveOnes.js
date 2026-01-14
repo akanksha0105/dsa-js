e3137d16d9b635847c1de895e1281cf73c69e189
Akanksha <mittalakanksha0105@gmail.com>
2026-01-14 13:01:03 +0530

    14. Maximum Consecutive Ones

---
+++ arrays/maxConsecutiveOnes.js
@@ -0,0 +1,16 @@
+//T.C: O(N)
+//S.C. : O(1)
+
+const findMaxConsecutiveOnes = (nums) => {
+	let currentCount = 0;
+	let maxCount = 0;
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[i] == 1) {
+			currentCount++;
+		} else {
+			maxCount = Math.max(currentCount, maxCount);
+			currentCount = 0;
+		}
+	}
+	return Math.max(maxCount, currentCount);
+};
