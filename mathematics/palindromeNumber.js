341f6ad79a37939707a6ef220052c8e12d5e0883
Akanksha <mittalakanksha0105@gmail.com>
2026-01-29 17:49:25 +0530

    29. Palindrome Number

---
+++ mathematics/palindromeNumber.js
@@ -0,0 +1,11 @@
+const isPalindrome = (x) => {
+	if (x < 0) return false;
+	let xcopy = x;
+	let rev = 0;
+	while (x > 0) {
+		let rem = x % 10;
+		rev = rev * 10 + rem;
+		x = Math.floor(x / 10);
+	}
+	return rev === xcopy;
+};
