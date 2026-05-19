e4317dfd585f4bcbf13a9f4c6652128c28208909
Akanksha <mittalakanksha0105@gmail.com>
2026-05-19 17:43:12 +0530

    Revision : Problem 5 : Second Largest

---
+++ revision/arrays/secondLargestElement.js
@@ -0,0 +1,16 @@
+
+const secondLargest = (nums) => {
+	let largest = -Infinity,
+		secondLargest = -Infinity;
+
+	for (let i = 0; i < nums.length; i++) {
+		if (largest < nums[i]) {
+			secondLargest = largest;
+			largest = nums[i];
+		} else if (largest > nums[i] && nums[i] > secondLargest) {
+			secondLargest = nums[i];
+		}
+	}
+
+	return secondLargest === -Infinity ? -1 : secondLargest;
+};
