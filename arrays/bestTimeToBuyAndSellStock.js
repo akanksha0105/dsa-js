55671981ddcee6fad4b315d844e7b96294fd4b9a
Akanksha <mittalakanksha0105@gmail.com>
2026-05-19 18:22:17 +0530

    Optimized the approach

--- arrays/bestTimeToBuyAndSellStock.js
+++ arrays/bestTimeToBuyAndSellStock.js
@@ -1,16 +1,36 @@
 const maxProfit = (prices) => {
-    let l = 0,
-            r = 1;
-        let maxP = 0;
+	let minPrice = prices[0];
+	let maxProfit = 0;
 
-        while (r < prices.length) {
-            if (prices[l] < prices[r]) {
-                let profit = prices[r] - prices[l];
-                maxP = Math.max(maxP, profit);
-            } else {
-                l = r;
-            }
-            r++;
-        }
-        return maxP;
-}
+	for (let i = 1; i < prices.length; i++) {
+		if (minPrice < prices[i]) {
+			let profit = prices[i] - minPrice;
+			maxProfit = Math.max(profit, maxProfit);
+		} else {
+			minPrice = prices[i];
+		}
+	}
+
+	return maxProfit;
+};
+
+
+
+
+
+// const maxProfit = (prices) => {
+//     let l = 0,
+//             r = 1;
+//         let maxP = 0;
+
+//         while (r < prices.length) {
+//             if (prices[l] < prices[r]) {
+//                 let profit = prices[r] - prices[l];
+//                 maxP = Math.max(maxP, profit);
+//             } else {
+//                 l = r;
+//             }
+//             r++;
+//         }
+//         return maxP;
+// }
